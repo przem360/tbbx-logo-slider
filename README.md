99335a3dfd948f919f06545be755350285bb36bf
Przemo <Przemo>
2017-11-27 05:06:18 +0100

    adding links to images

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # tbbx-logo-slider
 Simple image ticker for wordpress
 
-# usage
+## usage
 1. Download, install and activate plugin in WordPress
 2. Go to "Assosiates logos" (WordPress main admin menu)
 3. Add images as CPT
 4. Add [tls-shortcode] to page or post
+
+##
+
+0.0.2 - 27-11-2017: url metabox for adding links to logos
+0.0.1 - 26-11-2017: initial version
--- tbbx-logo-slider.php
+++ tbbx-logo-slider.php
@@ -3,7 +3,7 @@
     Plugin Name: Logo Slider
     Description: Simple marketing coops logo slider
     Author: Przemysław Wolski
-    Version: 1.0
+    Version: 0.0.2
 */
 
 /* update wordpress jQuery */
@@ -25,12 +25,76 @@ function tls_init() {
         'supports' => array(
             'title',
             'thumbnail'
-        )
+        ),
+		'register_meta_box_cb' => 'tls_metaboxes'
     );
     register_post_type('tls_images', $args);
 }
 add_theme_support( 'post-thumbnails' );
 add_action('init', 'tls_init');
+
+/* adding custom meta box for link url */
+function tls_metaboxes() {
+	add_meta_box(
+		'tls_url', // id
+		'url', // name
+		'tls_logo_url', // name of html function
+		'tls_images', // name of custom post type
+		'normal', // position of field: normal - in main column, side - in side panel
+		'default' // priority: high, low, default
+	);
+}
+// meta box html
+function tls_logo_url() {
+	global $post;
+	// Nonce field to validate form request came from current site
+	wp_nonce_field( basename( __FILE__ ), 'tls_logo' );
+	// Get the url data if it's already been entered
+	$linkurl = get_post_meta( $post->ID, 'url', true );
+	// Output the field
+	echo '<input type="text" name="url" value="' . esc_textarea( $linkurl )  . '" class="widefat">';
+}
+// save meta box data
+function tls_logo_url_save( $post_id, $post ) {
+	// Return if the user doesn't have edit permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return $post_id;
+	}
+	// Verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times.
+	if ( ! isset( $_POST['url'] ) || ! wp_verify_nonce( $_POST['tls_logo'], basename(__FILE__) ) ) {
+		return $post_id;
+	}
+	// Now that we're authenticated, time to save the data.
+	// This sanitizes the data from the field and saves it into an array $logo_url_meta.
+	$logo_url_meta['url'] = esc_textarea( $_POST['url'] );
+	// Cycle through the $logo_url_meta array.
+	// Note, in this example we just have one item, but this is helpful if you have multiple.
+	foreach ( $logo_url_meta as $key => $value ) :
+		// Don't store custom data twice
+		if ( 'revision' === $post->post_type ) {
+			return;
+		}
+		if ( get_post_meta( $post_id, $key, false ) ) {
+			// If the custom field already has a value, update it.
+			update_post_meta( $post_id, $key, $value );
+		} else {
+			// If the custom field doesn't have a value, add it.
+			add_post_meta( $post_id, $key, $value);
+		}
+		if ( ! $value ) {
+			// Delete the meta key if there's no value
+			delete_post_meta( $post_id, $key );
+		}
+	endforeach;
+}
+add_action( 'save_post', 'tls_logo_url_save', 1, 2 );
+
+
+
+
+
+
 
 /* registering scripts and styles */
 add_action('wp_print_scripts', 'tls_register_scripts');
@@ -74,9 +138,13 @@ function tls_function($type='tls_function') {
     $loop = new WP_Query($args);
     while ($loop->have_posts()) {
         $loop->the_post();
- 
+		$linkurl = get_post_meta( get_the_id(), 'url', true );
         $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);
+		if (!$linkurl) {
         $result .='<li><img title="'.get_the_title().'" src="' . $the_url[0] . '" data-thumb="' . $the_url[0] . '" alt=""/></li>';
+		} else {
+		$result .='<li><a href="'.$linkurl.'" target="_blank"><img title="'.get_the_title().'" src="' . $the_url[0] . '" data-thumb="' . $the_url[0] . '" alt=""/></a></li>';	
+		}
     }
 	$result .= '</ul>';
     $result .= '</div>';
@@ -84,7 +152,7 @@ function tls_function($type='tls_function') {
 }
 
 /* add shortcode */
-add_shortcode('tls-shortcode', 'tls_function');
+add_shortcode('tbbx-logo-slider', 'tls_function');
 
 /* widget */
 function tls_widgets_init() {
